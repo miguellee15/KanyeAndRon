158f042fc760e54d14e6b3b7f8b7c950a5c43d8c
miguellee15 <miguellee15@gmail.com>
2021-01-12 20:30:18 -0500

    Update

---
+++ KanyeandRon/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeandRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuotesGen.KanyeQuote();
+
+            QuotesGen.RonQuote();
+        }
+
+    }
+}
